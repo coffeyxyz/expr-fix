83b980bf2b4f02fa09a7eda9dfeaa6fe1670b071
foggynight <foggynight709@gmail.com>
2021-04-28 23:35:29 -0230

    Add token_t

---
+++ src/token.h
@@ -0,0 +1,12 @@
+// Copyright (C) 2021 Robert Coffey
+// All rights reserved
+
+#ifndef TOKEN_H
+#define TOKEN_H
+
+typedef struct token {
+	char *str;
+	unsigned char flags;
+} token_t;
+
+#endif // TOKEN_H
