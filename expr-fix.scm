3d3c904757c6e0426e8bfb665968abfa98125aa7
Robert Coffey <foggynight709@gmail.com>
2021-10-14 15:15:03 -0230

    Add expression lexer

--- expr-fix.scm
+++ expr-fix.scm
@@ -1,13 +1,18 @@
 (import (chicken format)
         (chicken io)
         (chicken port)
-        (chicken process-context))
+        (chicken process-context)
+        (chicken string))
 
+;;; binary-tree ----------------------------------------------------------------
+
+;; Get the root of a binary tree.
 (define (root tree)
   (cond ((null? tree) '())
         ((list? tree) (car tree))
         (else tree)))
 
+;; Get the left branch of a binary tree.
 (define (left tree)
   (if (or (not (list? tree))
           (null? tree)
@@ -15,6 +20,7 @@
       '()
       (cadr tree)))
 
+;; Get the right branch of a binary tree.
 (define (right tree)
   (if (or (not (list? tree))
           (null? tree)
@@ -23,6 +29,7 @@
       '()
       (caddr tree)))
 
+;; Get the string representation of a traversal of a binary tree.
 (define (traverse order tree)
   (define (preorder tree)
     (unless (null? tree)
@@ -49,6 +56,42 @@
     (if (> len 0)
         (substring str 0 (- len 1))
         str)))
+
+;;; lexer ----------------------------------------------------------------------
+
+;; TODO Throw an error when STRING->TOKEN encounters an invalid number string.
+;; TODO Add support for tokens not separated by spaces.
+
+(define operator-characters '(#\+ #\- #\* #\/ #\( #\)))
+
+;; Convert a string into a token.
+(define (string->token str)
+  (if (and (= (string-length str) 1)
+           (member (string-ref str 0) operator-characters))
+      (list 'operator (string-ref str 0))
+      (list 'number (string->number str))))
+
+;; Get the type of a token.
+(define (token-type token)
+  (car token))
+
+;; Get the value of a token.
+(define (token-value token)
+  (cadr token))
+
+;; Determine if a token is of the type: operator.
+(define (token-operator? token)
+  (eq? 'operator (token-type token)))
+
+;; Determine if a token is of the type: number.
+(define (token-number? token)
+  (eq? 'number (token-type token)))
+
+;; Get a list of the tokens contained within an expression string.
+(define (lex-expr expr)
+  (map string->token (string-split expr)))
+
+;;; main -----------------------------------------------------------------------
 
 (define (main args)
   (define input-fix)
