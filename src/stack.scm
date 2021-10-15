904f9a2b482a5cb0c5491e192c84c8583d7e6173
Robert Coffey <foggynight709@gmail.com>
2021-10-15 16:28:19 -0230

    Update stack module

--- src/stack.scm
+++ src/stack.scm
@@ -1,12 +1,18 @@
 ;;;; stack.scm - Stack data type.
 
-;; Make a stack containing ELEMENTS.
-(define (make-stack . elements)
-  elements)
+(declare (unit stack))
 
-;; Check if STACK is empty.
-(define (stack-empty? stack)
-  (null? stack))
+;; Make a stack containing ELEMENTS.
+(define (make-stack elements)
+  (if (list? elements)
+      elements
+      (error "make-stack: Bad argument type: ELEMENTS must be a list")))
+
+;; Check if a stack is empty.
+(define stack-empty? null?)
+
+;; Get the length of a stack.
+(define stack-length length)
 
 ;; Push ELEMENT onto STACK and return STACK.
 (define (stack-push stack element)
@@ -15,11 +21,23 @@
 ;; Pop an element from STACK and return STACK.
 (define (stack-pop stack)
   (if (stack-empty? stack)
-      (error "stack-pop: Stack is empty.")
+      (error "stack-pop: STACK is empty")
       (cdr stack)))
+
+;; Pop N elements from STACK and return STACK.
+(define (stack-pop-n stack n)
+  (if (> n (stack-length stack))
+      (error "stack-pop-n: STACK contains less than N elements")
+      (list-tail stack n)))
 
 ;; Return the top element of STACK.
 (define (stack-top stack)
   (if (stack-empty? stack)
-      (error "stack-top: Stack is empty.")
+      (error "stack-top: STACK is empty")
       (car stack)))
+
+;; Return the Nth element from the top of the stack.
+(define (stack-top-n stack n)
+  (if (< n (stack-length stack))
+      (list-ref stack n)
+      (error "stack-top-n: STACK contains less than N+1 elements")))
