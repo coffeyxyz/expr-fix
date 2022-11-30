7bf4ea2c8ee7f34267c3b2909ed5c346778f156e
Robert Coffey <rjc@heretyk.xyz>
2022-11-29 23:12:55 -0330

    add postfix parser

--- xpr-fix.scm
+++ xpr-fix.scm
@@ -1,25 +1,23 @@
 ;; xpr-fix.scm - xpr-fix in CHICKEN Scheme
-;;
-;; GRAMMARS
-
-#|
-
-For all grammars, the following symbols are defined:
-
-    N -> ['+', '-']? ['0' - '9']+
-    O -> '+' | '-' | '*' | '/'
-
-PREFIX
-    E -> N | O ' ' E ' ' E
-
-POSTFIX
-
-INFIX
-
-|#
-
-;;
 ;; Copyright (C) 2022 Robert Coffey
+;; Released under the MIT license.
+
+;; Grammars --------------------------------------------------------------------
+;;
+;;     N -> ['+', '-']? ['0' - '9']+
+;;     O -> '+' | '-' | '*' | '/'
+;;
+;; Prefix
+;;
+;;     E -> N | O ' ' E ' ' E
+;;
+;; Postfix
+;;
+;;     I tried to find an LL grammar... just going to use a stack automaton.
+;;
+;; Infix
+;;
+;;------------------------------------------------------------------------------
 
 (import (chicken format)
         (chicken io))
@@ -31,11 +29,11 @@ INFIX
 (define-record-type token
   (%make-token t v)
   token?
-  (t token-t)  ; type: symbol
-  (v token-v)) ; value: number
+  (t tok-t)  ; type: symbol
+  (v tok-v)) ; value: number
 
 (define-record-printer (token tok out)
-  (fprintf out "#<token ~A ~A>" (token-t tok) (token-v tok)))
+  (fprintf out "#<token ~A ~A>" (tok-t tok) (tok-v tok)))
 
 (define (make-token t #!optional (v (void)))
   (cond ((not (symbol? t)) (error 'make-token "type must be a symbol" t))
@@ -65,46 +63,65 @@ INFIX
               (let ((op (lex-op word)))
                 (if op
                     (cons op (lex-words (cdr words)))
-                    (error 'lexer "invalid token" word)))))))
+                    (error 'lex-line "invalid token" word)))))))
   (lex-words (string-split line)))
 
 ;; parser ----------------------------------------------------------------------
 
 (define (parse-num tok)
   (if (match? tok 'n)
-      (token-v tok)
-      (error 'parser "failed to parse number" tok)))
+      (tok-v tok)
+      (error 'parse-num "failed to parse number" tok)))
 
 (define (parse-op tok)
   (if (match? tok 'o)
-      (token-v tok)
-      (error 'parser "failed to parse operator" tok)))
+      (tok-v tok)
+      (error 'parse-op "failed to parse operator" tok)))
 
-(define token-curr)
-(define token-rest)
+(define tok-curr)
+(define tok-rest)
 
 (define (match? tok type)
-  (eq? (token-t tok) type))
+  (eq? (tok-t tok) type))
 
 (define (next!)
-  (if (null? token-rest)
-      (error 'parser "invalid expression")
-      (begin (set! token-curr (car token-rest))
-             (set! token-rest (cdr token-rest)))))
+  (if (null? tok-rest)
+      #f
+      (begin (set! tok-curr (car tok-rest))
+             (set! tok-rest (cdr tok-rest))
+             #t)))
 
 (define (parse-prefix line)
   (define (parse-expr!)
-    (next!)
-    (cond ((match? token-curr 'n) (parse-num token-curr))
-          ((match? token-curr 'o) (list (parse-op token-curr)
-                                        (parse-expr!)
-                                        (parse-expr!)))
-          (else (error 'parser "failed to parse expression"))))
-  (set! token-rest (lex-line line))
+    (unless (next!) (error 'parse-expr! "missing tokens"))
+    (cond ((match? tok-curr 'n) (parse-num tok-curr))
+          ((match? tok-curr 'o) (list (parse-op tok-curr)
+                                      (parse-expr!)
+                                      (parse-expr!)))
+          (else (error 'parse-expr! "invalid token" tok-curr))))
+  (set! tok-rest (lex-line line))
   (let ((tree (parse-expr!)))
-    (if (null? token-rest)
+    (if (null? tok-rest)
         tree
-        (error 'parser "invalid expression"))))
+        (error 'parse-prefix "unparsed tokens remain"))))
+
+(define (parse-postfix line)
+  (define stk '())
+  (define (parse-expr!)
+    (when (next!)
+      (cond ((match? tok-curr 'n) (set! stk (cons (parse-num tok-curr) stk)))
+            ((match? tok-curr 'o)
+             (if (or (null? stk) (null? (cdr stk)))
+                 (error 'parse-expr! "missing arguments" (tok-v tok-curr))
+                 (set! stk (cons (list (tok-v tok-curr) (cadr stk) (car stk))
+                                 (cddr stk)))))
+            (else (error 'parse-expr! "invalid token" tok-curr)))
+      (parse-expr!)))
+  (set! tok-rest (lex-line line))
+  (parse-expr!)
+  (cond ((null? stk) (error 'parse-postfix "missing tokens"))
+        ((not (null? (cdr stk))) (error 'parse-postfix "too many tokens"))
+        (else (car stk))))
 
 ;; main ------------------------------------------------------------------------
 
